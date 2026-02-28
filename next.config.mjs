5a3ee2c07d8dd0a087ffed8fb2dbaa133e5bf122
Ashfaqazad <ashfaqahmedazad@gmail.com>
2026-02-28 21:36:22 +0500

    Remove backend rewrites and update ImageConverter to client-side only

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,59 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-
-      {
-      source: "/api/converted/:path*",
-      destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/convert/converted/:path*`,
-    },
-      {
-        source: "/convert/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/convert/:path*`,
-      },
-
-      {
-        source: "/api/:path((?!convert).*)",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
-      },
-
-    ];
-  },
-
   async redirects() {
     return [
+      // agar pehle /tools/slug routes thay aur ab /slug pe shift kar diya:
       { source: "/tools/:slug*", destination: "/:slug*", permanent: true },
-      { source: "/excel-to-pdf", destination: "/excel-pdf", permanent: true },
-      { source: "/blog/excel-to-pdf", destination: "/blog/excel-pdf", permanent: true },
     ];
   },
 };
 
 export default nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+
+//       {
+//       source: "/api/converted/:path*",
+//       destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/convert/converted/:path*`,
+//     },
+//       {
+//         source: "/convert/:path*",
+//         destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/convert/:path*`,
+//       },
+
+//       {
+//         source: "/api/:path((?!convert).*)",
+//         destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
+//       },
+
+//     ];
+//   },
+
+//   async redirects() {
+//     return [
+//       { source: "/tools/:slug*", destination: "/:slug*", permanent: true },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
 
